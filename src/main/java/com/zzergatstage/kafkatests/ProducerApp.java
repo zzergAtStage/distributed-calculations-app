9a3002d03e7ef1fbd427a906eb6a117a6ac7f4f6
zzergAtStage <hightemplar98@iclod.com>
2025-06-03 00:19:51 +0200

    dev: chores

--- src/main/java/com/zzergatstage/kafkatests/ProducerApp.java
+++ src/main/java/com/zzergatstage/kafkatests/ProducerApp.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 /**
